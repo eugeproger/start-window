74ab7a0d52b45e2c98df8d805657bb36789f2738
Euge <euge.itech@gmail.com>
2023-02-02 16:30:38 +0100

    pause

--- src/main/java/com/itproger/itprogerjavafxapp/database/DatabaseHandler.java
+++ src/main/java/com/itproger/itprogerjavafxapp/database/DatabaseHandler.java
@@ -2,6 +2,7 @@ package com.itproger.itprogerjavafxapp.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseHandler extends Configuration {
@@ -16,5 +17,42 @@ public class DatabaseHandler extends Configuration {
         databaseConnection = DriverManager.getConnection(connectionString, databaseUser, databasePassword);
 
         return databaseConnection;
+    }
+
+    public void singUpUser(
+            String firstName,
+            String lastName,
+            String userName,
+            String password,
+            String location,
+            String gender
+    ) {
+        String insert = "INSERT INTO " +
+                Constant.USERS_TABLE +
+                "(" +
+                Constant.USER_FIRST_NAME + ", " +
+                Constant.USER_LAST_NAME + ", " +
+                Constant.USER_USER_NAME + ", " +
+                Constant.USER_PASSWORD + ", " +
+                Constant.USER_LOCATION + ", " +
+                Constant.USERS_GENDER +
+                ")" +
+                "VALUES(?,?,?,?,?,?)";
+
+        try {
+            PreparedStatement preparedStatement = getDatabaseConnection().prepareStatement(insert);
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setString(3, userName);
+            preparedStatement.setString(4, password);
+            preparedStatement.setString(5, location);
+            preparedStatement.setString(6, gender);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.itproger.itprogerjavafxapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.j;
 
 
     opens com.itproger.itprogerjavafxapp to javafx.fxml;
