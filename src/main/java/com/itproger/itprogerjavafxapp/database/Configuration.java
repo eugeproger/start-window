337cce7d9d1c4af73769dbc44296d149cf544b79
Euge <euge.itech@gmail.com>
2023-02-03 11:00:37 +0100

    pouse

--- src/main/java/com/itproger/itprogerjavafxapp/database/Configuration.java
+++ src/main/java/com/itproger/itprogerjavafxapp/database/Configuration.java
@@ -2,8 +2,8 @@ package com.itproger.itprogerjavafxapp.database;
 
 public class Configuration {
 
-    protected String databaseHost = "localhost";
-    protected String databasePort = "127.0.0.1";
+    protected String databaseHost = "127.0.0.1";
+    protected String databasePort = "3306";
     protected String databaseUser = "root";
     protected String databasePassword = "22225555";
     protected String databaseName = "itproger";
