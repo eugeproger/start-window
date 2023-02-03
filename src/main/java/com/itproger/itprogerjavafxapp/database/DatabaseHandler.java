49e7c4c6b51deea5ad3d9e744531817a6a50566e
Euge <euge.itech@gmail.com>
2023-02-03 11:54:57 +0100

    #11 - Добавление пользователя в базу данных

--- src/main/java/com/itproger/itprogerjavafxapp/database/DatabaseHandler.java
+++ src/main/java/com/itproger/itprogerjavafxapp/database/DatabaseHandler.java
@@ -1,7 +1,10 @@
 package com.itproger.itprogerjavafxapp.database;
 
+import com.itproger.itprogerjavafxapp.User;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseHandler extends Configuration {
@@ -11,10 +14,41 @@ public class DatabaseHandler extends Configuration {
     public Connection getDatabaseConnection() throws ClassNotFoundException, SQLException {
         String connectionString = "jdbc:mysql://" + databaseHost + ":" + databasePort + "/" + databaseName;
 
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
         databaseConnection = DriverManager.getConnection(connectionString, databaseUser, databasePassword);
 
         return databaseConnection;
+    }
+
+    public void singUpUser(User user) {
+
+        String insert = "INSERT INTO " +
+                Constant.USERS_TABLE +
+                "(" +
+                Constant.USER_FIRST_NAME + ", " +
+                Constant.USER_LAST_NAME + ", " +
+                Constant.USER_USER_NAME + ", " +
+                Constant.USER_PASSWORD + ", " +
+                Constant.USER_LOCATION + ", " +
+                Constant.USERS_GENDER +
+                ")" +
+                "VALUES(?,?,?,?,?,?)";
+
+        try {
+            PreparedStatement preparedStatement = getDatabaseConnection().prepareStatement(insert);
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getLastName());
+            preparedStatement.setString(3, user.getUserName());
+            preparedStatement.setString(4, user.getPassword());
+            preparedStatement.setString(5, user.getLocation());
+            preparedStatement.setString(6, user.getGender());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
